72fbc00271de8d625739584e591c45883834bb3f
Alexey Zolotykh <alexey.zolotykh@infobip.com>
2019-05-08 04:46:54 +0300

    Developer experience example

--- Makefile
+++ Makefile
@@ -1,7 +1,17 @@
-.PHONY: default
-default:
+SHELL = /bin/sh
+.PHONY: run server client open
+
+run: server client open
 	@echo Running developer server...
-	go run *.go;
-build_client:
+
+open:
+	@echo openning browser
+	open 'http://localhost:8000'
+
+
+client:
 	@echo Start build client
 	cd client && npm start
+server:
+	@echo Start build server
+	go run *.go;
--- main.go
+++ main.go
@@ -1,43 +1,54 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"time"
+  "fmt"
+  "net/http"
+  "net/url"
+  "time"
 
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+  "github.com/labstack/echo"
+  "github.com/labstack/echo/middleware"
 )
 
 func main() {
-	e := echo.New()
-	e.Use(getLogger())
-	e.Renderer = Renderer()
-	e.Static("/", "public/static/")
-	e.GET("/api", apiHandler).Name = "api"
-	e.Logger.Fatal(e.Start(":8000"))
+  e := echo.New()
+
+  e.Use(getLogger())
+
+  g := e.Group("/")
+  g.Use(getProxy())
+
+  e.GET("/api", apiHandler).Name = "api"
+
+  e.Renderer = Renderer()
+  e.Logger.Fatal(e.Start(":8000"))
+}
+
+func getProxy() echo.MiddlewareFunc {
+  proxyUrl, _ := url.Parse("http://localhost:1234")
+  targets := []*middleware.ProxyTarget{{URL: proxyUrl,},}
+  return middleware.Proxy(middleware.NewRoundRobinBalancer(targets))
 }
 
 func getLogger() echo.MiddlewareFunc {
-	return middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: "method=${method}, uri=${uri}, status=${status}\n",
-	})
+  return middleware.LoggerWithConfig(middleware.LoggerConfig{
+    Format: "method=${method}, uri=${uri}, status=${status}\n",
+  })
 }
 
 func apiHandler(c echo.Context) error {
 
-	event, err := FromEchoContext(c)
+  event, err := FromEchoContext(c)
 
-	if err != nil {
-		return echo.NewHTTPError(echo.ErrBadRequest.Code, err)
-	}
+  if err != nil {
+    return echo.NewHTTPError(echo.ErrBadRequest.Code, err)
+  }
 
-	c.Response().Header().Add("Content-Disposition", getResponseFileName(event.Title, event.Start))
-	c.Response().Header().Set("Content-Type", "text/calendar")
-	return c.Render(http.StatusOK, "event.ics", event)
+  c.Response().Header().Add("Content-Disposition", getResponseFileName(event.Title, event.Start))
+  c.Response().Header().Set("Content-Type", "text/calendar")
+  return c.Render(http.StatusOK, "event.ics", event)
 }
 
 func getResponseFileName(title string, start time.Time) string {
-	return fmt.Sprintf("attachment; filename=\"%s-%s.ics\"", title, start.Format("20060102T0303pm"))
+  return fmt.Sprintf("attachment; filename=\"%s-%s.ics\"", title, start.Format("20060102T0303pm"))
 }
-
